d02e128d8ac237fe891ec4d7b8896a49c2838fd4
Wesley Dias <wesley.dias.dev@gmail.com>
2022-11-02 09:30:57 -0300

    feat: add router sign-up

--- index.js
+++ index.js
@@ -8,7 +8,11 @@ app.use(bodyParser.json())
 const usuarios = []
 const tweets = []
 
-
+app.post("/sign-up", (req, res) => {
+  usuarios.push(req.body)
+  res.json(req.body);
+  res.status(200);
+})
 
 
 app.listen(5000, () => {
