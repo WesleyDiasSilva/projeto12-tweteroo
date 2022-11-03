96cd92db5af5678411c2c8a40f2786b190bb294e
Wesley Dias <wesley.dias.dev@gmail.com>
2022-11-03 10:28:07 -0300

    feat: add router get /tweets for get all last tweets

--- index.js
+++ index.js
@@ -10,11 +10,27 @@ const tweets = []
 
 app.post("/sign-up", (req, res) => {
   usuarios.push(req.body)
-  res.json(req.body);
+  console.log(usuarios)
+  res.send('Cadastro realizado com sucesso!')
   res.status(200);
+})
+
+app.post("/tweets" , (req, res) => {
+  tweets.push(req.body);
+  res.send('Tweet salvo com sucesso!')
+})
+
+app.get( "/tweets",(req, res) => {
+  const tweetsResponse = []
+  const response = {data: tweetsResponse}
+  tweets.reverse().slice(0,10).forEach((t) => {
+    const avatar = usuarios.find(user => t.username === user.username)
+    tweetsResponse.push({username: t.username, avatar: avatar.avatar, tweet: t.tweet})
+  })
+  res.send(response)
 })
 
 
 app.listen(5000, () => {
-  console.log("Servidor rodando!")
+  console.log("Servidor rodando na porta: http://localhost:5000")
 })
