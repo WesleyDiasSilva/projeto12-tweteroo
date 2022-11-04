17e055541d147fea85361ee3cd6983bceb9731ad
Wesley Dias <wesley.dias.dev@gmail.com>
2022-11-04 20:02:22 -0300

    feat: add router tweets/username

--- index.js
+++ index.js
@@ -1,44 +1,76 @@
-import express from "express";
-import cors from "cors";
-const app = express();
+import express from 'express';
+import cors from 'cors';
+import chalk from 'chalk';
 
-app.use(cors());
+const app = express();
 app.use(express.json());
-const usuarios = [];
-const tweets = [];
+app.use(cors());
+
+const usuarios = [{
+  username: "Wesley", 
+  avatar: "https://t.ctcdn.com.br/MJFyxnOWYbXOWChcEWbT03PXkq4=/512x288/smart/filters:format(webp)/i329345.jpeg"
+}];
+
+const tweets = [{
+  username: "Wesley",
+  avatar: "https://t.ctcdn.com.br/MJFyxnOWYbXOWChcEWbT03PXkq4=/512x288/smart/filters:format(webp)/i329345.jpeg",
+  tweet: "Foco, força & fé!"
+}];
+
 
 app.post("/sign-up", (req, res) => {
-  usuarios.push(req.body);
-  res.status(201).send("Cadastro realizado com sucesso!");
-});
+  
+  let {username, avatar} = req.body;
+  username = username.replace(" ",'');
 
-app.post("/tweets", (req, res) => {
-  tweets.push(req.body);
-  res.status(201).send("Tweet salvo com sucesso!");
+  if(!username || !avatar){
+    res.status(400).send("Todos os campos são obrigatórios!");
+    return;
+  }
+
+  usuarios.push({username, avatar});
+  res.status(201).send("Usuário cadastrado com sucesso")
 });
 
 app.get("/tweets", (req, res) => {
-  const tweetsResponse = [{
-		username: "bobesponja",
-			avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-		  tweet: "eu amo o hub"
-	}];
-  const response =  tweetsResponse;
-  tweets
-    .reverse()
-    .slice(0, 10)
-    .forEach((t) => {
-      const avatar = usuarios.find((user) => t.username === user.username);
-      tweetsResponse.push({
-        username: t.username,
-        avatar: avatar.avatar,
-        tweet: t.tweet,
-      });
-    });
-    console.log(response)
-  res.send(response);
+  if(usuarios.length === 0){
+    res.status(400).send("Não foi possível no momento, tente mais tarde!")
+    return;
+  } 
+
+  const response = tweets.reverse().slice(0,10);
+  res.status(200).send(response)
 });
 
-app.listen(5000, () => {
-  console.log("Servidor rodando na porta: http://localhost:5000");
+app.post("/tweets", (req, res) => {
+  let username = req.headers.user;
+  const {tweet} = req.body;
+  username = username.replace(' ','');
+
+  if(!username || !tweet){
+    res.status(400).send("Todos os campos são obrigatórios!")
+    return;
+  } 
+
+  const user = usuarios.find(user => user.username === username)
+  
+  tweets.push({
+    username,
+    tweet,
+    avatar: user.avatar
+  });
+
+  res.status(201).send("ok");
 });
+
+app.get("/tweets/:username", (req, res) => {
+  let {username} = req.params;
+  username = username.replace("%20",'');
+  username = username.replace(" ",'');
+
+  const response = tweets.filter(t => t.username === username)
+
+  console.log(username)
+  console.log(usuarios)
+  res.send(response)
+})
