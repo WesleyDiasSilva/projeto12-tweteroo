b0e273473754d93e0dcd4f09ba2e7d49144314eb
Wesley Dias <wesley.dias.dev@gmail.com>
2022-11-04 16:20:50 -0300

    Finished

--- index.js
+++ index.js
@@ -1,36 +1,44 @@
-import express from 'express';
-import cors from 'cors'
-import bodyParser from 'body-parser';
+import express from "express";
+import cors from "cors";
 const app = express();
 
-app.use(cors())
-app.use(bodyParser.json())
-const usuarios = []
-const tweets = []
+app.use(cors());
+app.use(express.json());
+const usuarios = [];
+const tweets = [];
 
 app.post("/sign-up", (req, res) => {
-  usuarios.push(req.body)
-  console.log(usuarios)
-  res.send('Cadastro realizado com sucesso!')
-  res.status(200);
-})
+  usuarios.push(req.body);
+  res.status(201).send("Cadastro realizado com sucesso!");
+});
 
-app.post("/tweets" , (req, res) => {
+app.post("/tweets", (req, res) => {
   tweets.push(req.body);
-  res.send('Tweet salvo com sucesso!')
-})
+  res.status(201).send("Tweet salvo com sucesso!");
+});
 
-app.get( "/tweets",(req, res) => {
-  const tweetsResponse = []
-  const response = {data: tweetsResponse}
-  tweets.reverse().slice(0,10).forEach((t) => {
-    const avatar = usuarios.find(user => t.username === user.username)
-    tweetsResponse.push({username: t.username, avatar: avatar.avatar, tweet: t.tweet})
-  })
-  res.send(response)
-})
-
+app.get("/tweets", (req, res) => {
+  const tweetsResponse = [{
+		username: "bobesponja",
+			avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
+		  tweet: "eu amo o hub"
+	}];
+  const response =  tweetsResponse;
+  tweets
+    .reverse()
+    .slice(0, 10)
+    .forEach((t) => {
+      const avatar = usuarios.find((user) => t.username === user.username);
+      tweetsResponse.push({
+        username: t.username,
+        avatar: avatar.avatar,
+        tweet: t.tweet,
+      });
+    });
+    console.log(response)
+  res.send(response);
+});
 
 app.listen(5000, () => {
-  console.log("Servidor rodando na porta: http://localhost:5000")
-})
+  console.log("Servidor rodando na porta: http://localhost:5000");
+});
